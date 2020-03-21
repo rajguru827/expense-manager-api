47f5e5607bea94ef62e0dd249e63ecc89c9a4a7f
Gururaj <17214716+rajguru827@users.noreply.github.com>
2020-03-21 09:49:20 +0530

    Added enum for categgory type

--- api/models/category.js
+++ api/models/category.js
@@ -4,7 +4,11 @@ const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true, unique: true },
     description: { type: String, required: true },
-    type: { type: Number, required: true },
+    type: {
+        type: String,
+        enum: ['income', 'expense'],
+        required: true
+    },
     createdDate: { type: Date, required: true }
 });
 
